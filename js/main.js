e51d46680e33a6713d75a664e125d9c8f70f127e
Shikaga <shikaga@gmail.com>
2013-07-26 19:39:17 +0100

    Basic idea

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ function sub(number1,number2)
 }
 
 console.log(sub(5,4));
+
+function getWineLabel() {
+	var source = document.getElementById("entry-template").innerHTML;
+	var template = Handlebars.compile(source);
+	var context = {title: "My New Post", body: "This is my first post!"}
+	var html = template(context);
+	var div = document.createElement("span");
+	div.innerHTML = html;
+	return div;
+}
+
+function addWineLabelDiv(div) {
+	var wineLabel = getWineLabel();
+	div.appendChild(wineLabel);
+}
